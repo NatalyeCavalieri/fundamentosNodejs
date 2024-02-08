5e7ed3e5a4fd53e2e64927f75f07965c0d3ec579
Natalye <cavalieridev@gmail.com>
2024-02-08 10:02:39 +0100

    create database

--- src/server.js
+++ src/server.js
@@ -1,25 +1,31 @@
 import http from "node:http"
 import { json } from "./middlewares/json.js"
+import { Database } from "./database.js"
+import { randomUUID } from 'node:crypto'
 
-const users = []
+const database = new Database()
+
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
 
   await json(request, response)
 
   if (method === "GET" && url === "/users") {
-    return response
-      .end(JSON.stringify(users))
+    const users = database.select("users")
+    return response.end(JSON.stringify(users))
   }
+
   if (method === "POST" && url === "/users") {
     const { name, email } = request.body
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+    database.insert("users", user)
     return response.writeHead(201).end()
   }
+
   return response.writeHead(404).end("NOT FOUND")
 })
 
